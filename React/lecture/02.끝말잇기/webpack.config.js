90ad17a889b10dbdd7b1d36622e4f58793d093f5
zena <syglee7@gmail.com>
2020-01-27 22:23:23 +0900

    2020.01.27 webpack lecture with wordrelay

--- React/lecture/02.끝말잇기/webpack.config.js
+++ React/lecture/02.끝말잇기/webpack.config.js
@@ -17,8 +17,19 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], //browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel',
+                ],
             },
         }]
     },
