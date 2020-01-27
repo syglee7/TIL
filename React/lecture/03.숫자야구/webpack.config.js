af71593ad79a87df78cc7968a97b64b8825de889
zena <syglee7@gmail.com>
2020-01-28 00:39:43 +0900

    2020.01.28 numberbaseball game

---
+++ React/lecture/03.숫자야구/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    name: 'word-relay-setting',
+    mode: 'development', //실서비스 : production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    }, // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'], //browserslist
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-hot-loader/babel',
+                ],
+            },
+        }]
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    }, // 출력
+};
