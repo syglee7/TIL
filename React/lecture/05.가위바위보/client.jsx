1b31a71ddb14552d0e714b313bac9d669682425b
zena <syglee7@gmail.com>
2020-01-28 23:02:13 +0900

    2020.01.28 react RSP game

---
+++ React/lecture/05.가위바위보/client.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const { hot } = require('react-hot-loader/root');
+
+const RSP = require('./RSP');
+
+const Hot = hot(RSP);
+
+ReactDom.render(<Hot />, document.querySelector('#root'));
