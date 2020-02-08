2d25189373d49a098a55ebbae5aa89f7a557a5a6
zena <syglee7@gmail.com>
2020-02-08 20:24:22 +0900

    2020.02.08 socket io chatroom test

--- Nodejs/lecture/gif-chat/socket.js
+++ Nodejs/lecture/gif-chat/socket.js
@@ -1,6 +1,9 @@
-const WebSocket = require('ws');
+//const WebSocket = require('ws');
+const SocketIO = require('socket.io');
+const axios = require('axios');
 
-module.exports = (server) => {
+module.exports = (server, app, sessionMiddleware) => {
+    /* 웹소켓
     const wss = new WebSocket.Server({ server });
 
     wss.on('connection', (ws, req) => {
@@ -27,5 +30,84 @@ module.exports = (server) => {
 
         ws.interval = interval;
 
+    });*/
+
+    const io = SocketIO(server, { path: '/socket.io' });
+    app.set('io', io); // 익스프레스 변수 저장 방법
+    // req.app.get('io').of('/room').emit
+    // 네임 스페이스
+    // io.of('/') 기본값
+
+    const room = io.of('/room');
+    const chat = io.of('/chat');
+
+    // 익스프레스 미들웨어를 소켓 IO 에서 쓰는 방법
+    io.use((socket, next) => {
+       sessionMiddleware(socket.request, socket.request.res, next);
     });
+
+    room.on('connection', (socket) => {
+       console.log('room 네임 스페이스에 접속');
+       socket.on('disconnect', () => {
+           console.log('room 네임스페이스 접속 해제');
+       });
+    });
+
+    chat.on('connection', (socket) => {
+       console.log('chat 네임 스페이스에 접속');
+       const req = socket.request;
+       const roomId = referer
+           .split('/')[referer.split('/').length - 1]
+           .replace(/\?.+/, '');
+
+       socket.join(roomId); // 방에 접속
+       socket.to(roomId).emit('join', {
+          user: 'system',
+          chat: `${req.session.color}님이 입장 하셨습니다.`
+       });
+
+       socket.on('disconnect', () => {
+          console.log('chat 네임 스페이스 접속 해제');
+          socket.leave(roomId); // 방 나가기
+           // 방에 인원이 하나도 없으면 방을 없애기
+          const currentRoom = socket.adapter.rooms[roomId];
+          const userCount = currentRoom ? currentRoom.length : 0;
+          if (userCount === 0) {
+              axios.delete(`http://localhost:8005/room/${roomId}`)
+                  .then(() => {
+                      console.log('방 제거 요청 성공');
+                  })
+                  .catch((error) => {
+                      console.error(error);
+                  });
+          } else {
+              socket.to(roomId).emit('exit', {
+                 user: 'system',
+                 chat: `${req.session.color}님이 퇴장 하셨습니다.`,
+              });
+          }
+       });
+    });
+
+   /* io.on('connection', (socket) => {
+        const req = socket.request;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
+        socket.on('disconnect', () => {
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
+        });
+        socket.on('error', (error) => {
+            console.error(error);
+        });
+        socket.on('message', (data) => {
+            console.log(data);
+        });
+        socket.on('reply', (data) => {
+            console.log(data);
+        });
+        socket.interval = setInterval(() => {
+            socket.emit('news', 'Hello Socket.IO');
+        }, 3000);
+    });*/
 };
