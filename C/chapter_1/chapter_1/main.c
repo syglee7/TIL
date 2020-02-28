80fc2b0eec775952b44af5c9d87a578c94e26e69
zena <syglee7@gmail.com>
2020-02-28 22:20:31 +0900

    chapter2

--- C/chapter_1/chapter_1/main.c
+++ C/chapter_1/chapter_1/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int a;
+
 int main(void) {
-	printf("Hello World!\n");
+
+	printf("The number is %d. \n", a);
 	system("pause");
 	return 0;
 }
+
+/*
+int 억단위 4byte
+long long 숫자가 긴 정수형을 표현할 때 사용 8byte
+double 일반적인 실수 8byte
+string 문자열을 표현할 때 사용
+bool 참/거짓 1bit
+char 한 문자를 표현할 때 사용 1byte
+*/
